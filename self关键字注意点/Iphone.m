e7797d5e5df2dee77b76dd8399147dc6c5699296
LCJ-MinYa <1049468118@qq.com>
2017-08-21 10:34:43 +0800

    self关键字的注意点

--- self关键字注意点/Iphone.m
+++ self关键字注意点/Iphone.m
@@ -14,6 +14,17 @@
  self会自动区分类方法和对象方法，如果在类方法使用self调用对象方法，那么会直接报错
  同理,self如果在对象方法中调用类方法,一样报错
  总之,记住self如果是类调用就代表类,在对象方法中调用就代表对象
+ 
+ 使用场景:
+ 类方法之间相互调用
+ 对象方法之间相互调用
+ 
+ 可以用于区分成员变量和局部变量同名的情况
+ -(void)setCpu:(int)cpu
+ {
+    cpu = cpu; 方式一有警告
+    self->cpu = cpu; 方式二无问题
+ }
  */
 +(void)carameWithFlahLightStates:(flahLightStates)status
 {
